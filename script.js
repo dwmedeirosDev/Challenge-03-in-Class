fb420aac81f3c4ca6656ccae76b5b7fc89338b1e
dwmedeiros94 <davidmedeiros94@hotmail.com>
2022-06-20 10:24:45 -0300

    Done

---
+++ script.js
@@ -0,0 +1,35 @@
+let food = "Lasanha"
+switch (food){
+    case "Lasanha":
+        console.log("Não vendemos esta comida")
+    break;
+    case "Pizza":
+        console.log("Estamos com escassez de pizza")
+    break;
+    case "Sorvete":
+        console.log("Aqui está, são 3 reais")
+    break;
+    default:
+        console.log("Erro!!!!")
+    break;
+}
+
+//##########################
+
+let perfume = "Amadeirados"
+switch (perfume){
+    case "Amadeirados":
+        console.log("Compra efetuada com sucesso")
+    break;
+    case "Florais":
+        console.log("Tem certeza que não prefere outro aroma?")
+    break;
+    case "Cítricos":
+        console.log("Tem certeza que não prefere outro aroma?")
+    break;
+    default:
+        console.log("Não trabalhamos com este tipo de perfume aqui")
+    break;
+}
+
+
